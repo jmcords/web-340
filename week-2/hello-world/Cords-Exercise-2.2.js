574c874cb791db1e4f508b38623761aff5abe9fc
Jason Cords <jmcords@my365.bellevue.edu>
2020-05-04 15:59:04 -0500

    Exercise 2.2

---
+++ week-2/hello-world/Cords-Exercise-2.2.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title:  Exercise-2.2.js
+; Author: Professor Krasso
+; Date:   5 MAY 2020
+; Modified by: Jason Cords
+; Description: Working with Express.
+=============================================
+*/
+
+const header = require('../../Cords-header.js');
+
+console.log(header.display("Jason", "Cords", "Exercise 1.3"),"\n"); //header
+
+var express = require("express"); //requires express module
+
+var http = require("http"); //requires http protocol
+
+var app = express(); //calls express function to start app
+
+app.use(function(request, response) //function passed request and response and logs/shows result
+{
+  console.log("in comes a request to: " + request.url);
+  response.end("Hello World!");
+});
+
+http.createServer(app).listen(8080);
